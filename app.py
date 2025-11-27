2279cabbb907d98e4b93be944ac5941ff6c5399f
kiran34254 <127660553+kiran34254@users.noreply.github.com>
2025-11-27 23:28:30 +0530

    Initialize Flask app with a custom route

---
+++ app.py
@@ -0,0 +1,11 @@
+from flask import Flask
+import os
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return f"Hello from Custom Server! Running as user: {os.getenv('APP_USER')}"
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8080)
